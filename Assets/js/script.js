6efb91b54c1d5ee97afadd2fcf57a7de83a8f2e4
Kayte McDonough <kaytemcdonough@gmail.com>
2021-02-22 19:31:48 -0500

    js edit to present day and date

---
+++ Assets/js/script.js
@@ -0,0 +1,8 @@
+$(function () {} );
+
+	//current date and time at the top of the page
+var today = moment().format("dddd, MMMM Do");
+
+var now = moment().format("H A");
+
+$("#currentDay").text(today);
