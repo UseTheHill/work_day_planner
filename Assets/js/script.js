fb11f854c8095319fabf8ed9462262cb1ae77c1f
Kayte McDonough <kaytemcdonough@gmail.com>
2021-02-22 19:57:04 -0500

    worked on js but then erased it to learn more, stepping away

--- Assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function () {} );
-
-	//current date and time at the top of the page
-var today = moment().format("dddd, MMMM Do");
-
-var now = moment().format("H A");
-
-$("#currentDay").text(today);
