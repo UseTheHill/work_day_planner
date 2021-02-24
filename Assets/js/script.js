152d8458529114f3bb5088912579858170987cd4
Kayte McDonough <kaytemcdonough@gmail.com>
2021-02-24 14:05:43 -0500

    added to js save call in js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,53 +1,72 @@
-$(function () {});
+$(function () {} );
 
-//variables
-let today = moment().format("dddd,MMM Do");
+	//variables
+var today = moment().format("dddd, MMMM Do");
 
-let now = moment().format("H A");
+var now = moment().format("H A");
 
-let planner = [
-    {time: "9 AM", event: ""},
-    {time: "10 AM", event: ""},
-    {time: "11 AM", event: ""},
-    {time: "12 PM", event: ""},
-    {time: "1 PM", event: ""},
-    {time: "2 PM", event: ""},
-    {time: "3 PM", event: ""},
-    {time: "4 PM", event: ""},
-    {time: "5 PM", event: ""},
+var planner = [
+	{time: "9 AM", event: "" },
+	{time: "10 AM", event: "" },
+	{time: "11 AM", event: "" },
+	{time: "12 AM", event: "" },
+	{time: "1 PM", event: "" },
+	{time: "2 PM", event: "" },
+	{time: "3 PM", event: "" },
+	{time: "4 PM", event: "" },
+	{time: "5 PM", event: "" }
 ];
 
-// get localstorage
-let events = JSON.parse(localStorage.getItem("workDay"));
-
+	//get localstorage
+var events = JSON.parse(localStorage.getItem("workDay"));
 if (events) {
-    planner = events;
-};
+	planner = events;
+}
 
-//display current day and date
-$('#currentDay').text(today);
+	//current date displayed
+$("#currentDay").text(today);
 
-//timeblock
+	//timeblock
 planner.forEach(function(timeBlock, index) {
-    let timeLabel = timeBlock.time;
-    let blockColor = colorRow(timeLabel);
-    let row = '<div class="time-block" id="' + index + 
+	var timeLabel = timeBlock.time
+	var blockColor = colorRow(timeLabel);
+	var row = 
+	'<div class="time-block" id="' + index + 
 	'"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
 	timeLabel + '</div><textarea class="form-control ' + blockColor + '">' + timeBlock.event +
 	'</textarea><div class="col-sm col-lg-1 input-group-append"><button type="button" class="saveBtn btn-block"><i class="fas fa-save"></i></button></div></div></div>';
+
+	$(".container").append(row);
 });
 
-$(".container").append(row);
-
-//color coded for past, present and future
+	//color time past, present, future 
 function colorRow(time) {
-    let planNow = moment(now, "H A");
-    let planEntry = moment(time, "H A");
-    if (planNow.isBefore(planEntry) === true) {
-        return "future";
-    } else if (planNow.isAfter(planEntry) === true) {
-        return "past";
-    } else {
-        return "present";
-    }
-};
+	var planNow = moment(now, "H A");
+	var planEntry = moment(time, "H A");
+	if (planNow.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planNow.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
+
+	//save 
+$(".saveBtn").on("click", function() {
+	var block = parseInt(
+		$(this)
+		.closest(".time-block")
+		.attr("id")
+	);
+	var entry = $.trim(
+		$(this)
+		.parent()
+		.siblings("textarea")
+		.val()
+	);
+	planner[block].event = entry
+	
+		//set localstorage
+	localStorage.setItem("workDay", JSON.stringify(planner))
+});
