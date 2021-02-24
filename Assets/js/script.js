53d7d3f505971178be2403b187dd72feffcbdf76
Kayte McDonough <kaytemcdonough@gmail.com>
2021-02-24 13:45:57 -0500

    variables added to js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,18 @@
-$(document).ready(function(){
+$(function () {});
 
-}
+//variables
+let today = moment().format("ddd,MMM Do");
+
+let now = moment().format("H A");
+
+let planner = [
+    {time: "9 AM", event: ""},
+    {time: "10 AM", event: ""},
+    {time: "11 AM", event: ""},
+    {time: "12 PM", event: ""},
+    {time: "1 PM", event: ""},
+    {time: "2 PM", event: ""},
+    {time: "3 PM", event: ""},
+    {time: "4 PM", event: ""},
+    {time: "5 PM", event: ""},
+];
